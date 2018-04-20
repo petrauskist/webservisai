b1928cfaecc542ad08526ac0253cd4dba29088cd
petrauskist <32622960+petrauskist@users.noreply.github.com>
2018-04-20 04:42:48 +0300

    DB conn + query

---
+++ WebServisai/WebServisai/Forma.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="Forma" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Forma.aspx.cs" Inherits="WebServisai.Forma" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <form id="form1" runat="server">
+        <p>Please select your preferred contact method:</p>
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:RadioButton type="radio" runat="server" ID="fc1" name="contact" value="email"/>
+            <label for="contactChoice1">Email</label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <input type="radio" id="fc2" name="contact" value="phone">
+            <label for="contactChoice2">Phone</label>
+
+            <input type="radio" id="contactChoice3" name="contact" value="mail">
+            <label for="contactChoice3">Mail</label>
+        </div>
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Button" />
+        </div>
+    </form>
+    
+</asp:Content>
---
+++ WebServisai/WebServisai/Forma.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebServisai
+{
+    public partial class Forma : Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           /* SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Vartotojas\Documents\GitHub\Webservisai\WebServisai\WebServisai\App_Data\Duombaze.mdf;Integrated Security=True");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(Rezultatas) FROM Rezultatai WHERE Rezultatas = @r", conn);
+            cmd.Parameters.AddWithValue("@r", TextBox1.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            cmd = new SqlCommand("INSERT INTO Rezultatai (Vardas,Rezultatas) VALUES (@v, @r)", conn);
+            cmd.Parameters.AddWithValue("@v", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@r", TextBox2.Text);
+            cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            conn.Close();*/
+        }
+    }
+}
